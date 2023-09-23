ed5a079a9208f963e09b364ba4dc0d9f3912e2b0
Amreenkhatoon <143897417+Amreenkhatoon@users.noreply.github.com>
2023-09-23 16:03:53 +0530

    Assignment_03

---
+++ Question 2.py
@@ -0,0 +1,9 @@
+def reverse_string(input_str):
+    reversed_str = input_str[::-1]
+    return reversed_str
+
+sample_string = "1234abcd"
+
+result = reverse_string(sample_string)
+
+print(result)
---
+++ question 3.py
@@ -0,0 +1,17 @@
+def count_upper_lower_characters(input_str):
+    upper_count = 0
+    lower_count = 0
+
+    for char in input_str:
+        if char.isupper():
+            upper_count += 1
+        elif char.islower():
+            lower_count += 1
+
+    return f"No. of upper case characters: {upper_count}\nNo. of lower case characters: {lower_count}"
+
+sample_string = "The quick Brow Fox"
+
+result = count_upper_lower_characters(sample_string)
+
+print(result)
